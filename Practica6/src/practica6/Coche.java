58dcb5623931809b96a8e3a86ef8f5f1c0e9bd72
PABLO <PABLO@DESKTOP-MHJTQ14>
2019-06-21 13:18:08 +0200

    Cambio de nombre de variables atributos y métodos por otros mas explicativos

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,38 +1,38 @@
 package practica6;
 
+
 public class Coche {
-	//Estos atributos necesitan getters y setters.
 	String matricula; 
-	String atrib;//Cambiamos el nombre del atributo atrib, que es poco explicativo, por Combustible que seria un atributo mucho mas coherente.
+	String combustible;
 	String modelo;
 	String fabricante;
 
 	public Coche() { 
-		matricula = "";  
-		atrib = "";
+		matricula = ""; 
+		combustible = "";
 		modelo = "";
 		fabricante = "";
 	}
 
-	public Coche(String m, String c, String mo, String f) {// Cambiamos los atributos del constructor coche por otros más significativos.
-		//Ponemos this. antes de las variables de la clase para no dar lugar a error.
-		matricula = m;
-		atrib = c; 
-		modelo = mo;
-		fabricante = f;
+	public Coche(String matricula, String combustible, String modelo, String fabricante) {
+		
+		this.matricula = matricula;
+		this.combustible = combustible; 
+		this.modelo = modelo;
+		this.fabricante = fabricante;
 	} 
 
-	public String metodo1() {//Los metodos son poco explicativos, le cambiamos el nombre por otro que ayude a entender mejor lo que hacen.
+	public String muestraCoche() {
 		String resultado = "";
-		if (atrib == "Gasolina") {
-			resultado += "El coche con matricula " + matricula + " "; //
-			resultado += metodoA(modelo, fabricante);//Cambiamos el nombre del metodoA por GastoCocheGasolina
-		} else if (atrib == "Diesel") {
+		if (combustible == "Gasolina") {
+			resultado += "El coche con matricula " + matricula + " "; 
+			resultado += GastoCocheGasolina(modelo, fabricante);
+		} else if (combustible == "Diesel") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);//Cambiamos el nombre del metodoB por GastoCocheDiesel
-		} else if (atrib == "Híbrido") {
+			resultado += GastoCocheDiesel(modelo, fabricante);
+		} else if (combustible == "Híbrido") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);//Cambiamos el nombre del metodoC por GastoCocheHibrido
+			resultado += GastoCocheHibrido(modelo, fabricante);
 		} else {
 			resultado += "El coche con matricula " + matricula + " ";
 			resultado += "no dispone de información";
--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -4,15 +4,11 @@ public class Moto {
 	String matricula;
 	int cilindrada;
 	
-	public Moto() {//Esto solamente inicializa las variables, pero no tiene ninguna función por lo que podemos suprimirlo.
-		matricula = "";
-		cilindrada = 0; 
-	}
 	
 	 
-	public Moto(String m, int c) {//Cambiamos las variables del constructor por otras mas significativas
-		matricula = m;//Ponemos this. antes de las variables de la clase para que no de lugar a error.
-		cilindrada = c;
+	public Moto(String matricula, int cilindrada) {//Cambiamos las variables del constructor por otras mas significativas
+		this.matricula = matricula;//Ponemos this. antes de las variables de la clase para que no de lugar a error.
+		this.cilindrada = cilindrada;
 	}
 
 	public String getMatricula() {
@@ -23,11 +19,11 @@ public class Moto {
 		return cilindrada;
 	}
 
-	public void setMatricula(String m) {//Ponemos this. antes de la variable de la clase y cambiamos la string del set por una mas significativa
-		matricula = m;
+	public void setMatricula(String matricula) {//Ponemos this. antes de la variable de la clase y cambiamos la string del set por una mas significativa
+		this.matricula = matricula;
 	}
 
-	public void setCilindrada(int c) {//Ponemos this. antes de la variable de la clase y cambiamos el int del set por uno mas significativo
-		cilindrada = c;
+	public void setCilindrada(int cilindrada) {//Ponemos this. antes de la variable de la clase y cambiamos el int del set por uno mas significativo
+		this.cilindrada = cilindrada;
 	}
 }
