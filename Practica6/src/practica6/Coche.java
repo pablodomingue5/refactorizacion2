0fcb870b826e837bcb4d3de4ee6ea3ef2b649cf9
PABLO <PABLO@DESKTOP-MHJTQ14>
2019-06-21 12:34:56 +0200

    Detectados los malos olores.

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,36 +1,38 @@
 package practica6;
 
 public class Coche {
-	String matricula;
-	String atrib;
+	//Estos atributos necesitan getters y setters.
+	String matricula; 
+	String atrib;//Cambiamos el nombre del atributo atrib, que es poco explicativo, por Combustible que seria un atributo mucho mas coherente.
 	String modelo;
 	String fabricante;
 
 	public Coche() { 
-		matricula = "";
+		matricula = ""; //Podemos poner 
 		atrib = "";
 		modelo = "";
 		fabricante = "";
 	}
 
-	public Coche(String m, String c, String mo, String f) {
+	public Coche(String m, String c, String mo, String f) {// Cambiamos los atributos del constructor coche por otros más significativos.
+		//Ponemos this. antes de las variables de la clase para no dar lugar a error.
 		matricula = m;
-		atrib = c;
+		atrib = c; 
 		modelo = mo;
 		fabricante = f;
-	}
+	} 
 
-	public String metodo1() {
+	public String metodo1() {//Los metodos son poco explicativos, le cambiamos el nombre por otro que ayude a entender mejor lo que hacen.
 		String resultado = "";
 		if (atrib == "Gasolina") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
+			resultado += "El coche con matricula " + matricula + " "; //
+			resultado += metodoA(modelo, fabricante);//Cambiamos el nombre del metodoA por GastoCocheGasolina
 		} else if (atrib == "Diesel") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
+			resultado += metodoB(modelo, fabricante);//Cambiamos el nombre del metodoB por GastoCocheDiesel
 		} else if (atrib == "Híbrido") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
+			resultado += metodoC(modelo, fabricante);//Cambiamos el nombre del metodoC por GastoCocheHibrido
 		} else {
 			resultado += "El coche con matricula " + matricula + " ";
 			resultado += "no dispone de información";
--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -3,14 +3,15 @@ package practica6;
 public class Moto {
 	String matricula;
 	int cilindrada;
-
-	public Moto() {
+	
+	public Moto() {//Esto solamente inicializa las variables, pero no tiene ninguna función por lo que podemos suprimirlo.
 		matricula = "";
-		cilindrada = 0;
+		cilindrada = 0; 
 	}
-
-	public Moto(String m, int c) {
-		matricula = m;
+	
+	
+	public Moto(String m, int c) {//Cambiamos las variables del constructor por otras mas significativas
+		matricula = m;//Ponemos this. antes de las variables de la clase para que no de lugar a error.
 		cilindrada = c;
 	}
 
@@ -22,11 +23,11 @@ public class Moto {
 		return cilindrada;
 	}
 
-	public void setMatricula(String m) {
+	public void setMatricula(String m) {//Ponemos this. antes de la variable de la clase y cambiamos la string del set por una mas significativa
 		matricula = m;
 	}
 
-	public void setCilindrada(int c) {
+	public void setCilindrada(int c) {//Ponemos this. antes de la variable de la clase y cambiamos el int del set por uno mas significativo
 		cilindrada = c;
 	}
 }
